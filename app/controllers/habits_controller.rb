305298b3ac16e1715f7da069841439f30f5ff073
John S <diaz.john312@gmail.com>
2018-11-24 19:40:25 -0500

    habits controller

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,5 +1,43 @@
 class HabitsController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  # GET /
   def index
+    # please check and update habits status
+    @habits = current_user.habits
     render 'habits/index'
+  end
+
+  # POST /habits/new
+  def create
+    if !user_signed_in?
+      return head :unauthorized
+    end
+
+    @habit = current_user.habits.new(habit_params)
+
+    if @habit.save
+      # day should be deleted if it was completed...
+      render json: {}, status: :created
+    else
+      render json: {}, status: :unprocessable_entity
+    end
+  end
+
+  # POST /habits/:id/complete
+  # def complete
+  #   habit = Habit.find(params[:id])
+  #   return render 'habits/index', alert: 'Habit does not exist' if !habit
+  #   habit.complete = true
+  #   if habit.save
+  #     # check to update day
+  #     render 'habits/index'
+  #   else
+  #     render 'habits/index', alert: 'Could not save changes'
+  #   end
+  # end
+
+  private
+  def habit_params
+    params.require(:habit).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'habits#index'
+  get '/', to: 'habits#index', as: 'habits_index'
+  post '/habits/:id/complete', to: 'habits#complete'
+  post '/habits/new', to: 'habits#create'
+
   devise_for :users, controllers: {
     sessions: 'user/sessions',
     registrations: 'user/registrations'
